36fc9777df39376c616e1d5f862c902d2441b888
Catalina Londoño <nilocuru@hotmail.com>
2021-07-16 01:02:43 -0500

    first commit

---
+++ db.js
@@ -0,0 +1,62 @@
+const { Pool } = require('pg')
+
+let pool = {}
+
+async function connect () {
+  pool = new Pool({
+    user: 'postgres',
+    host: process.env.DBHOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: 5432
+  })
+  try {
+    const res = await pool.query('SELECT NOW()')
+    return res
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function querySalesByClient (clientId) {
+  const text = 'select s.id, c."name" as "clientName", p."name" as "productName", s."amount", s."date", p."price", p2."name" as "provider" from sales s, products p, clients c, providers p2 where s.client_id = $1 and s.client_id = c.id and s.product_id = p.id and p.provider_id = p2.id'
+  const values = [ clientId ]
+  const res = await pool.query(text, values)
+  return res.rows
+}
+
+async function querySalesByProvider (providerId) {
+  const text = 'select s.id, p2."name" as "providerName", p."name" as "productName", s."amount", s."date", p.price, c."name" as "clientName" from sales s, products p, clients c, providers p2 where p2.id = $1 and s.client_id = c.id and s.product_id = p.id and p.provider_id = p2.id '
+  const values = [ providerId ]
+  const res = await pool.query(text, values)
+  return res.rows
+}
+
+async function querySalesByProduct (productId) {
+  const text = 'select s.id, p."name" as "productName", p2."name" as "providerName", s."amount", s."date", c."name", p."price" as "clientName" from sales s, products p, clients c, providers p2 where p.id = $1 and s.client_id = c.id and s.product_id = p.id and p.provider_id = p2.id'
+  const values = [ productId ]
+  const res = await pool.query(text, values)
+  return res.rows
+}
+
+async function querySalesDetail (saleId) {
+  const text = 'select s.id, s."date", s.amount, c."name" as "clientName", c.username, p."name" as "productName", p.price, p.reference from sales s, clients c, products p where s.id = $1 and c.id = s.client_id and p.id = s.product_id '
+  const values = [ saleId ]
+  const res = await pool.query(text, values)
+  return res.rows
+}
+
+function disconnect () {
+  pool.end().then(() => {
+    console.log('Pool has ended')
+  })
+}
+
+module.exports = {
+  connect,
+  disconnect,
+  querySalesByClient,
+  querySalesByProvider,
+  querySalesByProduct,
+  querySalesDetail
+}
---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+require('dotenv').config()
+const DB = require('./db')
+const cors = require('cors')
+
+const app = express()
+const port = 4000
+
+DB.connect()
+
+
+app.use(cors({maxAge: 2592000}))
+
+app.get('/client/:id', async (req, res) => {
+    const saleId = req.params.id
+    const resultSet = await DB.querySalesByClient(saleId)
+    console.log('Queried ' + resultSet.length + ' rows')
+    res.status(200).send(resultSet)
+})
+
+app.get('/product/:id', async (req, res) => {
+    const saleId = req.params.id
+    const resultSet = await DB.querySalesByProduct(saleId)
+    console.log('Queried ' + resultSet.length + ' rows')
+    res.status(200).send(resultSet)
+})
+
+app.get('/provider/:id', async (req, res) => {
+    const saleId = req.params.id
+    const resultSet = await DB.querySalesByProvider(saleId)
+    console.log('Queried ' + resultSet.length + ' rows')
+    res.status(200).send(resultSet)
+})
+
+app.get('/sale/:id', async (req, res) => {
+    const saleId = req.params.id
+    const resultSet = await DB.querySalesDetail(saleId)
+    console.log('Queried ' + resultSet.length + ' rows')
+    res.status(200).send(resultSet)
+})
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port)
+})
+
+process.on('exit', () => {
+    console.log('Shutting down server, disconnecting from DB')
+    DB.disconnect()
+})
